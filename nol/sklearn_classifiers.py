29be0ac4dc9c6a9ca9d97305bd0374044abb841c
Tim LaRock <timothylarock@gmail.com>
2019-06-13 21:33:24 -0400

    Added seperate sklearn classifiers.

---
+++ nol/sklearn_classifiers.py
@@ -0,0 +1,71 @@
+from scipy import stats
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from puAdapter import *
+
+def compute_svm_values(samples_mat, unprobed_features, unprobedNodeIndices):
+    features = samples_mat[:,0:samples_mat.shape[1]-1]
+    y = samples_mat[:,samples_mat.shape[1]-1]
+    model = SVC(probability=False, kernel = 'poly')
+    model = model.fit(features, y)
+    new_values =dict()
+    for node in unprobedNodeIndices:
+        new_values[node] = model.decision_function(unprobed_features[node].reshape(1,-1))
+
+    return new_values
+
+def compute_knn_values(samples_mat, unprobed_features, unprobedNodeIndices):
+    features = samples_mat[:,0:samples_mat.shape[1]-1]
+    y = samples_mat[:,samples_mat.shape[1]-1]
+    model = KNeighborsClassifier()
+    model = model.fit(features, y)
+    new_values =dict()
+    for node in unprobedNodeIndices:
+        new_values[node] = model.predict_proba(unprobed_features[node].reshape(1,-1))[0][1]
+
+
+    return new_values
+
+def compute_logit_values(samples_mat, unprobed_features, unprobedNodeIndices, one_class=False):
+    features = samples_mat[:,0:samples_mat.shape[1]-1]
+    y = samples_mat[:,samples_mat.shape[1]-1]
+    model = LogisticRegression()
+
+    if one_class:
+        model = PUAdapter(model, hold_out_ratio=0.1)
+        model.fit(features, y)
+    else:
+        model = model.fit(features, y)
+
+
+    values_arr = model.predict_proba(unprobed_features)
+
+    if one_class:
+        values = {node:values_arr[node] for node in unprobedNodeIndices}
+        new_theta = np.zeros(features.shape[1])
+    else:
+        values = {node:values_arr[node][1] for node in unprobedNodeIndices}
+        new_theta = np.array(model.coef_).T
+
+
+    new_theta = new_theta.reshape((new_theta.shape[0],))
+    return values, new_theta
+
+def compute_linreg_values(samples_mat, unprobed_features, unprobedNodeIndices):
+    features = samples_mat[:,0:samples_mat.shape[1]-1]
+    y = samples_mat[:,samples_mat.shape[1]-1]
+    model = LinearRegression()
+    model = model.fit(features, y)
+
+    new_theta = np.array(model.coef_).T
+
+    values_arr = unprobed_features.dot(new_theta)
+
+    values = {node:values_arr[node] for node in unprobedNodeIndices}
+    return values, new_theta
+
+def compute_deg_values(G, unprobedNodeIndices):
+    deg_values = {node:len(G.sample_graph_adjlist[G.row_to_node[node]]) for node in unprobedNodeIndices}
+    return deg_values
+
