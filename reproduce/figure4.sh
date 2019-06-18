8f50f23675ef255f33bf6feebea9bb3adb3df598
tlarock <larock.t@husky.neu.edu>
2019-06-18 12:51:52 -0400

    Fixed typo in figure4.sh

--- reproduce/figure4.sh
+++ reproduce/figure4.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-processes=1
+processes=$1
 
-bash reproduce_dataset 'lfr-1' $processes
-bash reproduce_dataset 'lfr-2' $processes
-bash reproduce_dataset 'lfr-3' $processes
-bash reproduce_dataset 'lfr-4' $processes
+bash reproduce_dataset.sh 'lfr-1' $processes
+bash reproduce_dataset.sh 'lfr-2' $processes
+bash reproduce_dataset.sh 'lfr-3' $processes
+bash reproduce_dataset.sh 'lfr-4' $processes 'wait'
+
+wait
 
 ## PLOTTING CODE
