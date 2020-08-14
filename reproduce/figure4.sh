826aa09c07a46ae8ed44bc611c3578679d619be9
Tim LaRock <timothylarock@gmail.com>
2020-08-14 08:31:25 -0400

    Update figure4.sh

--- reproduce/figure4.sh
+++ reproduce/figure4.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# First argument to this script should be an integer
+# that will determine the number of parallel processes 
+# used per experiment
 processes=$1
 
 bash reproduce_dataset.sh 'twitter' node $processes 'wait'
