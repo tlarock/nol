6cfba577dc44791d493fe1e8036cacddf3625f8f
Tim LaRock <timothylarock@gmail.com>
2020-08-14 08:30:41 -0400

    Update figure3.sh

--- reproduce/figure3.sh
+++ reproduce/figure3.sh
@@ -3,13 +3,19 @@
 # the highest level directory. e.g. export NOL=path/to/nol
 set -ex
 
+# First argument to this script should be an integer
+# that will determine the number of parallel processes 
+# used per experiment
 processes=$1
 
-bash reproduce_dataset.sh ba node $processes 'wait' &
-bash reproduce_dataset.sh bter node $processes 'wait' &
-bash reproduce_dataset.sh cora node $processes 'wait' &
-bash reproduce_dataset.sh dblp node $processes 'wait' &
-bash reproduce_dataset.sh caida node $processes 'wait' &
+# If you want to run on all networks at once,
+# just add an ampersand (&) to the end of all but the
+# last line
+bash reproduce_dataset.sh ba node $processes 'wait'
+bash reproduce_dataset.sh bter node $processes 'wait'
+bash reproduce_dataset.sh cora node $processes 'wait'
+bash reproduce_dataset.sh dblp node $processes 'wait'
+bash reproduce_dataset.sh caida node $processes 'wait'
 bash reproduce_dataset.sh enron node $processes 'wait' 
 
 wait
